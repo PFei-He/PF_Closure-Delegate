36a0093422b85799597c4909facae1deaf333b49
PFei_He <498130877@qq.com>
2014-08-18 14:03:00 +0800

    Initial Commit

---
+++ PF_Closure&Delegate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PF_Closure&Delegate
+//
+//  Created by PFei_He on 14-8-18.
+//  Copyright (c) 2014年 PFei_He. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
